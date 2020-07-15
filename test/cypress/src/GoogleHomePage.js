7c472fc401655b669180efb41806e85fb45219ad
Omar <Omar>
2020-07-15 12:33:11 +0300

    add more tests for spec file

--- test/cypress/src/GoogleHomePage.js
+++ test/cypress/src/GoogleHomePage.js
@@ -1,10 +1,23 @@
 import BasePage from './BasePage';
 
 export class GoogleHomePage extends BasePage {
-	// common locators
-	googleCenterLogo = '#hplogo';
+  // common locators
 
-	path = '/';
+  feelingLuckyButton = 'div[class="FPdoLc tfB0Bf"] input[class="RNmpXc"]';
+  gmailButton = '[class="gb_g"][data-pid="23"]';
+  gmailLogo = '.h-c-header--product-marketing-one-tier .h-c-header__bar .h-c-header__lockup';
+  googleAppsButton = '#gbwa a';
+  googleCenterLogo = '[id="hplogo"]';
+  googleImagesLogo = '[class="logo-subtext"]';
+  imagesButton = '[class="gb_g"][data-pid="2"]';
+  latestTitle = '[id="latest-title"]';
+  searchField = 'input[class="gLFyf gsfi"]';
+  searchList = 'ul[class="erkvQe"]';
+  searchResult = '[id="result-stats"]';
+  socialSharingIcons = '[class="card"] [id="sharel"] a';
+  topDateBox = '[class="card"]';
+
+  path = '/';
 }
 
 export const google = new GoogleHomePage();
