f9066de721f32f113ebe0b7a8824ee8b8c1ebb47
OmarQaddourah <omarqadd96@gmail.com>
2022-01-20 13:14:43 +0200

    update some tests.

--- test/cypress/integration/google-homepage.func-spec.js
+++ test/cypress/integration/google-homepage.func-spec.js
@@ -9,46 +9,9 @@ describe('Desktop Google Homepage Functional Checks', () => {
 	});
 
 	describe('Center Logo', () => {
-		it('should be displayed', () => {
+		it('should be displayed and have correct src attribute', () => {
 			google.getElement(google.googleCenterLogo).should('be.visible');
-		});
-	});
-
-	describe('Search Field', () => {
-		it('should be displayed', () => {
-			google.getElement(google.searchField).should('be.visible');
-		});
-
-		it('should display list of search after type "google"', () => {
-			google.getElement(google.searchField).type('google');
-			google.getElement(google.searchList).should('be.visible');
-		});
-
-		it('should display search results after type "google" and click enter', () => {
-			google.getElement(google.searchField).clear().type('google{enter}');
-			google.getElement(google.searchResult).should('be.visible').then(($element) => {
-				const text = $element.text();
-				expect(text).to.contain('نتيجة'); // sometimes "results" instead of "نتيجة" or vice versa
-			});
-			cy.go('back');
-		});
-	});
-
-	describe('Feeling Lucky Button', () => {
-		it('should redirect to "doodles" page', () => {
-			google.clickElement(google.feelingLuckyButton);
-			cy.url().then(($url) => {
-				expect($url).to.contain('doodles');
-			});
-		});
-
-		it('should display date box that includes text and social sharing icons', () => {
-			google.getElement(google.topDateBox).should('be.visible');
-			google.getElement(google.latestTitle).should('be.visible');
-			google.getElement(google.socialSharingIcons).each(($li) => {
-				cy.get($li).should('be.visible');
-			});
-			cy.go('back');
+			google.getElement(google.googleCenterLogo).invoke('attr', 'src').should('contain', 'googlelogo');
 		});
 	});
 });
